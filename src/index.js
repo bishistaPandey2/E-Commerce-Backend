47689488ee02a6b808a3bc046ddb76ccbaf215b4
bishistaPandey2 <bishistapandey@gmail.com>
2025-11-18 21:11:43 +0545

    Create express app and use it after connecting database.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { connectDB } from "./db/database.js"; 
 import dotenv from "dotenv";
+import {app} from "./app.js"
 
 dotenv.config(
   {
@@ -9,3 +10,13 @@ dotenv.config(
 )
 
 connectDB()
+.then ( () => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`)
+    }) 
+  }
+)
+.catch ((err) => {
+      console.log('Server is not Running!', err)
+    }
+)
