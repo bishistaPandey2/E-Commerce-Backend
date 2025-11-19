aaa55ef27c441b6b72346973bc9c6f6a758a5929
bishistaPandey2 <bishistapandey@gmail.com>
2025-11-19 17:25:02 +0545

    Add all the utils, asyncHanlder and error handler to get started (II).

---
+++ src/middleware/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  try {
+    destination: function (req, file, cb) {
+      cb(null, './public/temp')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  }
+})
+
+const upload = multer({ storage: storage })
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+class ApiResponse {
+  constructr(statusCode, data, message = "success"){
+    this.statusCode = statusCode
+    this.data = data
+    this.message = message 
+    this.success = statusCode < 400 
+  }
+}
+
+export {ApiResponse}
